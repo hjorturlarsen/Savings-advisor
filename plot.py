888ac160064b734a716ad0cd0554d57297748851
Hjörtur Larsen <91hjortur@gmail.com>
2014-02-12 15:48:13 +0000

    Eytt út óþarfa texta

--- plot.py
+++ plot.py
@@ -21,25 +21,3 @@ def plot_reglulegurspar(greidsla, timabil, vextir):
     plt.ylabel("Kronur")
     plt.xlabel("Manudir")
     plt.show()
-
-#def plot_sparnadar_takmark(upphaed, eign, timabil, vextir):
-#    vex = vextir / 100.0
-#    plotData = []
-#    for manudur in range(1,timabil+1):
-#        efra = ((upphaed - eign * math.pow(1 + (vex / 12), (12 * manudur))) * vex / 12)
-#        nedra = math.pow(1 + (vex / 12), 12 * manudur) - 1
-#        plotData.append(round(efra/nedra))
-#    plt.plot(plotData)
-#    plt.ylabel("Kronur")
-#    plt.xlabel("Ar")
-#    plt.show()
-
-#def plot_sparnadar_timi(markmid, upphaed, vextir):
-#    vex = vextir / 100.0
-#    efra = math.log10(((markmid * (vex / 12)) / upphaed) + 1)
-#    nedra = math.log10(1 + (vex / 12)) * 12
-#	
-#    total = efra / nedra
-#    ar = int(total)
-#    manudir = math.ceil((total - ar) * 12)
-#    return ar, manudir
